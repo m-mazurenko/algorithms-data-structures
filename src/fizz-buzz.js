f0926c3aef80b20144e63cf9bcf8cb9ed9229943
nickolamaz <nickolay.ma@gmail.com>
2021-08-10 09:48:41 +0300

    Add fizz-buzz solution

---
+++ src/fizz-buzz.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    const res = []
+    for (let i = 1; i < n + 1; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            res.push('FizzBuzz');
+        } else if (i % 3 === 0) {
+            res.push('Fizz');
+        } else if (i % 5 === 0) {
+            res.push('Buzz');
+        } else {
+            res.push(i.toString());
+        }
+    }
+    return res;
+};
+
+console.log(fizzBuzz(3));
+console.log(fizzBuzz(5));
+console.log(fizzBuzz(15));
