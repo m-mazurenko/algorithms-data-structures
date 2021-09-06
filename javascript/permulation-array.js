97300cba4e0a951afb098c63301c4a64dceecb19
nickolamaz <nickolay.ma@gmail.com>
2021-09-06 09:03:05 +0300

    Add permulation-array solution

---
+++ javascript/permulation-array.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/build-array-from-permutation/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+  let ans = [];
+  for (let i = 0; i < nums.length; i += 1) {
+    ans.push(nums[nums[i]])
+  }
+  return ans;
+};
+
+
+console.log(buildArray([0,2,1,5,3,4]))
+console.log(buildArray([5,0,1,2,3,4]))
